0a9179126031da17bcacea2f4523e68c642be49b
asm2025 <asm2do@outlook.com>
2025-06-15 17:51:02 +0300

    Refactor parkable function to use Arc and Condvar for thread signaling

--- session02/parking/src/main.rs
+++ session02/parking/src/main.rs
@@ -1,9 +1,18 @@
+use std::{
+    sync::{Arc, Condvar, Mutex},
+    thread,
+};
 use util::ninput;
 
-fn parkable(n: u32) {
+fn parkable(n: usize, signal: Arc<(Mutex<bool>, Condvar)>) {
     loop {
-        std::thread::park();
+        thread::park();
         println!(">>> Thread {n} was unparked briefly.");
+
+        let (lock, cvar) = &*signal;
+        let mut done = lock.lock().unwrap();
+        *done = true;
+        cvar.notify_one();
     }
 }
 
@@ -11,19 +20,43 @@ fn main() {
     let mut threads = vec![];
 
     for i in 0..10 {
-        threads.push(std::thread::spawn(move || {
-            parkable(i);
-        }));
+        let signal = Arc::new((Mutex::new(false), Condvar::new()));
+        let signal2 = signal.clone();
+        threads.push((
+            thread::spawn(move || {
+                parkable(i + 1, signal2);
+            }),
+            signal,
+        ));
     }
 
     loop {
-        let input = ninput(Some("Enter a number to unpark a thread (0 to exit): ")).unwrap_or(0);
+        let input =
+            ninput::<usize>(Some("Enter a number to unpark a thread (0 to exit): ")).unwrap_or(0);
 
         if input == 0 {
             break;
-        } else if input < threads.len() as u32 {
+        }
+
+        let index = input - 1;
+
+        if index < threads.len() {
+            let (handle, signal) = &threads[index];
+            let (lock, cvar) = &**signal;
+
+            {
+                let mut done = lock.lock().unwrap();
+                *done = false;
+            }
+
             println!("Unparking thread {input}.");
-            threads[input as usize].thread().unpark();
+            handle.thread().unpark();
+
+            let mut done = lock.lock().unwrap();
+
+            while !*done {
+                done = cvar.wait(done).unwrap();
+            }
         } else {
             println!("Invalid thread number: {input}. Please try again.");
         }
