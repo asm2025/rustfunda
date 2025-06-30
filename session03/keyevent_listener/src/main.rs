10835cfa802776eb6de7d9d123aee6df00759fd5
asm2025 <asm2do@outlook.com>
2025-06-30 06:22:42 +0300

    feat: Refactor key event listener for improved asynchronous handling and remove unnecessary loop

--- session03/keyevent_listener/src/main.rs
+++ session03/keyevent_listener/src/main.rs
@@ -2,31 +2,27 @@ use crossterm::{
     event::{self, Event, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
-use std::{
-    sync::{
-        Arc,
-        atomic::{AtomicBool, Ordering},
-    },
-    thread,
-    time::Duration,
-};
-use util::io::clear_keys;
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
-    let running = Arc::new(AtomicBool::new(true));
-    let running_clone = running.clone();
+    let (tx, rx) = mpsc::channel();
 
     // Spawn key listener thread
     thread::spawn(move || {
-        println!("Press any key to stop the loop...");
-        clear_keys();
         enable_raw_mode().unwrap();
 
         loop {
-            if event::poll(Duration::from_millis(100)).unwrap() {
-                if let Ok(Event::Key(_)) = event::read() {
-                    println!("\nKey pressed! Stopping loop...\n");
-                    running_clone.store(false, Ordering::Relaxed);
+            if let Ok(Event::Key(key)) = event::read() {
+                if !key.is_press() {
+                    continue;
+                }
+
+                if tx.send(key).is_err() {
+                    // Main thread dropped
+                    break;
+                }
+
+                if key.code == KeyCode::Esc {
                     break;
                 }
             }
@@ -35,13 +31,29 @@ fn main() {
         disable_raw_mode().unwrap();
     });
 
-    let mut counter = 0;
+    println!("Press keys (ESC to quit):");
 
-    while running.load(Ordering::Relaxed) {
-        println!("Loop iteration: {}", counter);
-        counter += 1;
-        thread::sleep(Duration::from_secs(1));
+    // Main thread continues without blocking
+    loop {
+        match rx.try_recv() {
+            Ok(key) => match key.code {
+                KeyCode::Esc => break,
+                KeyCode::Char(c) => {
+                    if key.modifiers.is_empty() {
+                        println!("Pressed: {}", c);
+                    } else {
+                        println!("Pressed: {} with {:?}", c, key.modifiers);
+                    }
+                }
+                _ => println!("Pressed: {:?} with {:?}", key.code, key.modifiers),
+            },
+            Err(mpsc::TryRecvError::Disconnected) => {
+                // Listener is disconnected
+                break;
+            }
+            Err(_) => {
+                thread::sleep(Duration::from_millis(10));
+            }
+        }
     }
-
-    println!("Loop finished!");
 }
