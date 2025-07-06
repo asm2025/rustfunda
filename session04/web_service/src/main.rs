def2c59a91334a51333460e66602c3d3f3b16bb0
asm2025 <asm2do@outlook.com>
2025-07-07 00:08:37 +0300

    refactor: Simplify host URI assignment in index.html

--- session04/web_service/src/main.rs
+++ session04/web_service/src/main.rs
@@ -15,6 +15,7 @@ async fn main() -> Result<()> {
     let app = create_router();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await?;
+
     Ok(())
 }
 
