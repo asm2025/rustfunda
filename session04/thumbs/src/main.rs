c1369be83ce85613518dcb6e59a2a793bd4e5781
asm2025 <asm2do@outlook.com>
2025-11-07 14:46:42 +0200

    update timeout and lifetime of the db connection

--- session04/thumbs/src/main.rs
+++ session04/thumbs/src/main.rs
@@ -148,10 +148,10 @@ async fn setup_database(db_url: &str) -> Result<DatabaseConnection> {
     let mut opt = ConnectOptions::new(db_url);
     opt.max_connections(100)
         .min_connections(5)
-        .connect_timeout(Duration::from_secs(8))
-        .acquire_timeout(Duration::from_secs(8))
-        .idle_timeout(Duration::from_secs(8))
-        .max_lifetime(Duration::from_secs(8));
+        .connect_timeout(Duration::from_secs(30))
+        .acquire_timeout(Duration::from_secs(30))
+        .idle_timeout(Duration::from_secs(300)) // 5 minutes
+        .max_lifetime(Duration::from_secs(1800)); // 30 minutes
 
     // Connect to the database
     let db = Database::connect(opt).await?;
